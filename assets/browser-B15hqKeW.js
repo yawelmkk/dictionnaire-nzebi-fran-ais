655e414199447a337146dc8b6d35cc57b50c82c9
yawelmkk <languenzebiofficiel@gmail.com>
2025-06-18 01:30:18 +0000

    Updates

---
+++ assets/browser-B15hqKeW.js
@@ -0,0 +1,1 @@
+import{h as e}from"./vendor-fWX8neT-.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
--- src/components/MobileNav.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-
-export function MobileNav() {
-  // Component disabled - no longer displaying the white band at the bottom
-  return null;
-}
